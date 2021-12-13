310713f3463c009e27c1645f01e5a3943743d3a1
lorenzo-guerrini <lorenzoguerrini8@live.it>
2021-12-13 04:26:06 +0100

    Milestone 3 completata

--- myscript.js
+++ myscript.js
@@ -1,20 +1,22 @@
 for (let i = 1; i <= 100; i++) {
     let number;
+    const container = document.querySelector(".container");
+    const element = document.createElement("div");
 
     if ((i % 3 == 0) && (i % 5 == 0)) {
         number = "FizzBuzz";
+        element.classList.add("multiple35")
     } else if (i % 3 == 0) {
         number = "Fizz";
+        element.classList.add("multiple3")
     } else if (i % 5 == 0) {
         number = "Buzz";
+        element.classList.add("multiple5")
     } else {
         number = i;
     }
 
     console.log(number);
-
-    const container = document.querySelector(".container");
-    const element = document.createElement("div");
 
     element.append(i);
     container.append(element);
